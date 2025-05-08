332223b3e296af217695323a35d3a784a9685820
aflah-sudo <aflahuse1@gmail.com>
2025-05-08 20:30:34 +0530

    fixed bugs

--- organizer/renamer.py
+++ organizer/renamer.py
@@ -7,27 +7,31 @@ class FileRenamer:
     for category, files in self.renamed_files.items():
         for file in files:
           answr=input("enter yes to use ai to rename else enter no")
-          if answr.lower()==yes:
-            from google import genai
-            print("entert your gemini 1.5 flash api key : (if you dont have one, get it from https://makersuite.google.com/app/apikey)")
-            api = input()
-            client = genai.Client(api_key=api)
-            if category == docs:
-              ext=os.path.splitext(file)[1]
-              if ext.lower()==txt:
-                content = open(file,"r").read()
-                if len(content)>1000:
-                  content=content[:1000]
-                response = client.models.generate_content(
-                    model="gemini-1.5-flash", contents=f"suugest a a suitable name for title for this file{content}"
-                )
-                name = f"{response.text}.txt"
-                os.rename(file,name)
-                print(f"changed file to {name}")
+          if answr.lower()=="yes":
+            airenamer(category, file)
           else:
-            name = f"{os.path.get.time(file)}.{os.path.splitext(file)[1]}"
+            name = f"{os.path.gettime(file)}.{os.path.splitext(file)[1]}"
             os.rename(file)
             print(f"file renamed to {name}")
+
+   def airenamer(self,category,file):
+    from google import genai
+    print("enter your gemini 1.5 flash api key : (if you don't have one, get it from https://makersuite.google.com/app/apikey)")
+    api = input()
+    client = genai.Client(api_key=api)
+    if category == "docs":
+        ext = os.path.splitext(file)[1].lower()
+        if ext in [".txt",".json"]:
+            content = open(file, "r").read()
+            if len(content) > 1000:
+                content = content[:1000]
+            response = client.models.generate_content(
+                model="gemini-1.5-flash", contents=f"suggest a suitable title for this file: {content}"
+            )
+            name = f"{response.text}{ext}"
+            os.rename(file, name)
+            print(f"changed file to {name}")
+
           
             
               
